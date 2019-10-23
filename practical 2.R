e75712f7d29e196429c3133f6cb37160f2fdd0c8
Diqiu Yang <diqiuyang@eduroam-int-dhcp-97-49-27.ucl.ac.uk>
2019-10-23 14:57:07 +0100

    1st

---
+++ practical 2.R
@@ -0,0 +1,25 @@
+getwd()
+mycsv<- read.csv("ward-profiles-excel-version.csv")
+1+5
+4*2^2
+A <- 1
+B <- 2
+C <- A+B
+C
+ 
+ls()
+
+rm(A)
+
+ls()
+x <- function(1,2,3,4)
+  
+data1 <- c(1:100)
+data2 <- c(101:200)
+plot(data1,data2,col="red")
+
+data3 <- rnorm(100,mean = 53, sd=1)
+data4 <- rnorm(100,mean=64,sd=1)
+plot(data3,data4,col="blue")
+
+?plot
